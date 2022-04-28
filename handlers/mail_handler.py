676af06c3ef1770f6ae2f40784a90e8661183bc0
Bernardo Meireles <67381633+dvntx@users.noreply.github.com>
2022-04-28 23:29:10 +0100

    Update mail_handler.py

--- handlers/mail_handler.py
+++ handlers/mail_handler.py
@@ -1,36 +1,36 @@
+from attr import asdict
 import requests
 import random, string
 
-BASE = "https://www.1secmail.com/api/v1/"
+base = "https://www.1secmail.com/api/v1/"
 working_domains = ["wwjmp.com",
                    "esiix.com",
                    "xojxe.com",
                    "yoggm.com"]
 
 def random_string(length):
-  return ''.join(random.choice(string.ascii_letters) for i in range(length))
+    return ''.join(random.choice(string.ascii_letters) for i in range(length))
 
 def gen_email():
-  return random_string(random.randint(10, 15)) + "@" + random.choice(working_domains)
+    return random_string(random.randint(10, 15)) + "@" + random.choice(working_domains)
 
 def get_inbox(email):
-  user, domain = email.split("@")
-  return requests.get(f"{BASE}?action=getMessages&login={user}&domain={domain}").json()
+    user, domain = email.split("@")
+    return requests.get(f"{base}?action=getMessages&login={user}&domain={domain}").json()
 
 def check_mail(inbox):
-  if len(inbox) > 0:
-    return True
-  else:
-    return False
+    if len(inbox) > 0:
+        return True
+    else:
+        return False
 
 def get_id(inbox):
-  return inbox[0]["id"]
+    return inbox[0]["id"]
 
 def retrieve_email(email, id):
-  user, domain = email.split("@")
-  return requests.get(f"{BASE}?action=readMessage&login={user}&domain={domain}&id={id}").json()["body"]
-  
+    user, domain = email.split("@")
+    return requests.get(f"{base}?action=readMessage&login={user}&domain={domain}&id={id}").json()["body"]
 
 def parse_otp(content):
-  locate = content.find('<p class="otp">')
-  return content[locate + 15: locate + 21]
+    locate = content.find('<p class="otp">')
+    return content[locate + 15: locate + 21]
